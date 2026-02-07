88e496c4e8d0eef877ab715f1cf8490c38307f4a
NguyenDThai <thainguyen4646@gmail.com>
2026-02-07 13:46:18 +0700

    fix middleware

--- middleware.ts
+++ middleware.ts
@@ -1,14 +1,27 @@
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
+import { jwtVerify } from "jose";
 
-export function middleware(req: NextRequest) {
-  const token = req.cookies.get("access_token");
+const secret = new TextEncoder().encode(process.env.JWT_SECRET);
+
+export async function middleware(req: NextRequest) {
+  const token = req.cookies.get("access_token")?.value;
 
   if (!token) {
     return NextResponse.redirect(new URL("/login", req.url));
   }
 
-  return NextResponse.next();
+  try {
+    const { payload } = await jwtVerify(token, secret);
+
+    if (payload.role !== "ADMIN") {
+      return NextResponse.redirect(new URL("/403", req.url));
+    }
+
+    return NextResponse.next();
+  } catch (error) {
+    return NextResponse.redirect(new URL("/login", req.url));
+  }
 }
 
 export const config = {
